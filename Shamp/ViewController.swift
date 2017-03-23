5fdfd1c415960871c69a6280fc8bbbe8a77a68c5
sergiobravotalero <seergio.bravo@koombea.com>
2017-03-23 12:12:54 -0500

    Initial Commit

---
+++ Shamp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shamp
+//
+//  Created by Sergio David Bravo Talero on 3/23/17.
+//  Copyright © 2017 Koombea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
