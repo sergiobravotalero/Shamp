75c8cbc5f0472603d35aa29884ecdf3c31c840d7
sergiobravotalero <seergio.bravo@koombea.com>
2017-05-21 19:33:37 -0500

    rating product

--- Shamp/ViewControllers/Home/Extensions/StampCellDelegate/StampCellDelegate.swift
+++ Shamp/ViewControllers/Home/Extensions/StampCellDelegate/StampCellDelegate.swift
@@ -7,11 +7,29 @@
 //
 
 import Foundation
+import  UIKit
 
 extension HomeViewController: StampCellDelegate {
     
     func userDidSelectStamp(stamp: Stamp) {
-        selectedStamp = stamp
-        performSegue(withIdentifier: "showShirts", sender: nil)
+        let actionController = UIAlertController(title: "Choose an option", message: nil, preferredStyle: .actionSheet)
+        
+        let addAction = UIAlertAction(title: "Select this stamp", style: .default, handler: { (action) in
+            self.selectedStamp = stamp
+            self.performSegue(withIdentifier: "showShirts", sender: nil)
+        })
+        actionController.addAction(addAction)
+        
+        if let productRating = SessionHandler.shared.listOfFeatures?.productRating, productRating{
+            let rateAction = UIAlertAction(title: "Rate this stamp", style: .default, handler: { (action) in
+            
+            })
+            actionController.addAction(rateAction)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        actionController.addAction(cancelAction)
+        
+        present(actionController, animated: true, completion: nil)
     }
 }
